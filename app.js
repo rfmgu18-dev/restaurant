40a46af2bd2cf98cbb530f539cec821aca897bbd
Ruben_fMg.dev <rfmgu18@gmail.com>
2026-08-02 19:23:57 -0400

    Grandes avances

---
+++ app.js
@@ -0,0 +1,64 @@
+const estatusPedido = document.querySelector('#parrafoEstatus');
+const resultadoPedido = document.querySelector('#resultadoPedido');
+const btnEnviarPedido = document.querySelector('#enviarPedido');
+const pizza = document.querySelector('#estatusPizza');
+const bebida = document.querySelector('#estatusBebida');
+const postre = document.querySelector('#estatusPostre');
+
+const estatusDelPedido = () => {
+    return Math.random() < 0.8;
+};
+
+const actualizarEstado = (elemento, mensaje) => {
+    elemento.textContent = mensaje;
+};
+
+const prepararItem = (elemento, mensaje, tiempo) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            actualizarEstado(elemento, mensaje + ' ✅');
+            resolve();
+        }, tiempo);
+    });
+};
+
+const preparacionPedido = async () => {
+    try {
+        await prepararItem(pizza, 'Pizza lista', 3000);
+        await prepararItem(bebida, 'Bebida lista', 3000);
+        await prepararItem(postre, 'Postre listo', 3000);
+        actualizarEstado(estatusPedido, 'Tu pedido está listo ✅');
+    } catch (error) {
+        console.log(error);
+    }
+
+    btnEnviarPedido.disabled = false;
+    btnEnviarPedido.textContent = 'Enviar pedido';
+};
+
+//Fase de espera
+btnEnviarPedido.addEventListener('click', () => {
+    actualizarEstado(estatusPedido, 'Procesando pedido...');
+    actualizarEstado(pizza, 'Preparando...');
+    actualizarEstado(bebida, 'Preparando...');
+    actualizarEstado(postre, 'Preparando...');
+
+    btnEnviarPedido.disabled = true;
+    btnEnviarPedido.textContent = 'Preparando...';
+
+    const pedidoAprobado = estatusDelPedido();
+
+    //Pedido rechazado
+    if (!pedidoAprobado) {
+        actualizarEstado(resultadoPedido, 'Pedido rechazado...');
+        resultadoPedido.className = 'resultado resultado--error';
+        actualizarEstado(estatusPedido, 'Ocurrió un error al preparar tu pedido. Porfavor, intente nuevamente ❌.');
+
+        return;
+    }
+
+    actualizarEstado(resultadoPedido, '¡Pedido aprobado! 🎉');
+    resultadoPedido.className = 'resultado resultado--exito';
+    actualizarEstado(estatusPedido, 'Tu pedido ha sido procesado con éxito ✅');
+    preparacionPedido();
+});
